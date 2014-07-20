7729582b08fd0fb5c5e570393c3f2ddca91ef36a
Vladimir Lugovsky <v.lugovsky@itransition.com>
2014-07-20 20:53:59 +0300

    Added levels page.

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,12 @@
 'use strict';
 
 define(['module', 'app/main'], function(module, main) {
-    main.register.controller(ngCName(module, 'homeController'), [function() {}]);
+    main.register.controller(ngCName(module, 'menuController'), [function() {}]);
+    main.register.controller(ngCName(module, 'levelsController'), ['$scope', function($scope) {
+        var res = [];
+        for (var i = 0; i < 20; i++) {
+            res.push({ id: i + 1 });
+        }
+        $scope.levels = res;
+    }]);
 });
