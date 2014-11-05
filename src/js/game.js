8b35f8d76b94c3617bdf5e8fad0b89153c926836
Vladimir Lugovsky <v.lugovsky@itransition.com>
2014-11-05 14:37:00 +0300

    Levels panel swiping.

--- src/js/game.js
+++ src/js/game.js
@@ -262,12 +262,4 @@ define(['module', 'app/main', 'angular'], function(module, main, angular) {
         }
     });
 
-    main.register.filter('range', function() {
-        return function(input, total) {
-            total = parseInt(total);
-            for (var i=0; i<total; i++)
-              input.push(i);
-            return input;
-        };
-    });
 });
