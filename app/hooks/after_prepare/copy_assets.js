8654e3315a7da28b7760dc6e4a41758ea1f09360
Vladimir Lugovsky <vl@flatlogic.com>
2014-11-25 21:39:07 +0300

    Other structure changes.

--- app/hooks/after_prepare/copy_assets.js
+++ app/hooks/after_prepare/copy_assets.js
@@ -19,16 +19,16 @@ var filestocopy = [{
     "www/icon.png": 
     "platforms/android/res/drawable/icon.png"
 }, {
-    "www/res/icon/android/icon-72-hdpi.png": 
+    "res/icon/android/icon-72-hdpi.png": 
     "platforms/android/res/drawable-hdpi/icon.png"
 }, {
-    "www/res/icon/android/icon-36-ldpi.png": 
+    "res/icon/android/icon-36-ldpi.png": 
     "platforms/android/res/drawable-ldpi/icon.png"
 }, {
-    "www/res/icon/android/icon-48-mdpi.png": 
+    "res/icon/android/icon-48-mdpi.png": 
     "platforms/android/res/drawable-mdpi/icon.png"
 }, {
-    "www/res/icon/android/icon-96-xhdpi.png": 
+    "res/icon/android/icon-96-xhdpi.png": 
     "platforms/android/res/drawable-xhdpi/icon.png"
 }, {
     "config/android/res/drawable/splash.png": 
